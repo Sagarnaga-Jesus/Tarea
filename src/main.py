883ce8ac856456802d00f6ded9c98370b8ff44ea
Sagarnaga-jesus <23308060610335@cetis61.edu.mx>
2026-04-20 09:03:11 -0500

    ahhhhhh

--- src/main.py
+++ src/main.py
@@ -5,6 +5,10 @@ from views.LoginView import LoginView
 from views.dashboardView import DashboardView
 
 def start(page: ft.Page):
+    page.title = "Sistema SIGE"
+    page.window_width = 450
+    page.window_height = 700
+    
     # instanciar controladores ua sola
     auth_ctrl = AuthController()
     task_ctrl = TareaController()
@@ -13,22 +17,29 @@ def start(page: ft.Page):
         page.views.clear()
         
         if page.route == "/":
-            page.add(ft.Text("Caso 1"))
             page.views.append(LoginView(page, auth_ctrl))
             
         elif page.route == "/dashboard":
             page.views.append(DashboardView(page, task_ctrl))
             
-        if not page.views:
-            page.views.append(
-                ft.View("/",[ft.Text("Error: Ruta no encontrada o vista vacia")])
-            )
-        
             #agregas aqui las vistas que necesites
         page.update()
-    page.on_route_change = route_change
 
-    page.go("/")
+    def view_pop(e):
+        if len(page.views) > 1:
+            page.views.pop()
+            page.update()
+            top_view = page.views[-1]
+            page.go(top_view.route)
+    
+    #Manejos de eventos
+    page.on_route_change = route_change
+    page.on_view_pop = view_pop
+    
+    if page.route == "/":
+        route_change(None)
+    else:
+        page.go("/")
     
 def main ():
     ft. app (target=start)
--- src/views/LoginView.py
+++ src/views/LoginView.py
@@ -1,10 +1,16 @@
 import flet as ft
 
 def LoginView(page, auth_controller):
-    email_input = ft.TextField(label="Correo Electrónico", width=350, border_radius=10)
-    pass_input = ft.TextField(label="Contraseña", width=350, password=True, can_reveal_password=True)
+    email_input = ft.TextField(label="Correo Electrónico", width=350, border_radius=10, keyboard_type=ft.KeyboardType.EMAIL)
+    pass_input = ft.TextField(label="Contraseña", width=350, password=True, can_reveal_password=True, border_radius=10)
     
     def login_click(e):
+        if not email_input.value or not pass_input.value:
+            page.snackbar = ft.Snackbar(ft.Text("Por favor, complete todos los campos"))
+            page.snackbar.open = True
+            page.update()
+            return
+        
         user, msg = auth_controller.login(email_input.value, pass_input.value)
         if user:
             page.session.set("user", user) # Guardar el usuario en la sesión
@@ -14,15 +20,27 @@ def LoginView(page, auth_controller):
             page.snackbar.open = True
             page.update()
         
-        return ft.View("/login", [
-            ft.AppBar(title=ft.Text("SIGE - Login"), bgcolor = ft.Color.BLUE_GREY_900, color="white"),
-            ft.Column([
-                ft.Icon(ft.Icons.LOCK_PERSON, size=50, color=ft.Color.BLUE),
-                ft.Text("Acceso al sistema ", size=24, weight="bold"),
-                email_input,
-                pass_input,
-                ft.ElevatedButton("Entrar", on_click=login_click, width=350),
-                ft.TextButton("Crear cuenta", on_click=lambda e: page.go("/register"))
-            ], horizontal_alignment=ft.CrossAxisAlignment.CENTER, alignment=ft.MainAxisAlignment.CENTER,
-                )
-        ])
+        login_button = ft.ElevatedButton("Entrar", on_click=login_click, width=350, bgcolor="blue", color="white")
+        
+        pass_input.on_submit = login_click
+        
+        return ft.View(
+            route="/", 
+            verical_alignment=ft.MainAxisAlignment.CENTER,
+            horizontal_alignment=ft.CrossAxisAlignment.CENTER,
+            appbar=ft.AppBar(title=ft.Text("SIGE - Login"), bgcolor = ft.Colors.BLUE_GREY_900, color="white"),
+            controls=[
+                ft.Column([
+                    ft.Text("Acceso al sistema ", size=24, weight="bold"),
+                    email_input,
+                    pass_input,
+                    login_button,
+                    ft.ElevatedButton("Entrar", on_click=login_click, width=350),
+                    ft.TextButton("Crear cuenta", on_click=lambda e: page.go("/registro"))
+                ],
+                horizontal_alignment=ft.CrossAxisAlignment.CENTER, alignment=ft.MainAxisAlignment.CENTER,
+                spacing=20,
+                tigtht=True
+            )
+        ]
+    )
